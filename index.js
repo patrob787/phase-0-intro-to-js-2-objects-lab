a29158d04c1eec9635727a8a1f3030012c9e5ee8
Patrick Roberts <patrob22@gmail.com>
2023-03-02 15:51:31 -0500

    obj lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Joel Pellini',
+    streetAddress: '123 New York St.',
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee,
+    [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+};
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+};
